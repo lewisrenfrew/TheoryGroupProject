9f9e79b3be5802a49e7f359de03c1df8baeac0fe
Goobley <2087801o@student.gla.ac.uk>
2016-01-22 00:36:14 +0000

    Do you even brain?
    Try to save the right file...

--- src/main.cpp
+++ src/main.cpp
@@ -1,242 +1,108 @@
 /* ==========================================================================
-   $File: Grid.cpp $
+   $File: grid.cpp $
    $Version: 1.0 $
-   $Notice: (C) Copyright 2016 Chris Osborne. All Rights Reserved. $
+   $Notice: (C) Copyright 2015 Chris Osborne. All Rights Reserved. $
    $License: MIT: http://opensource.org/licenses/MIT $
    ========================================================================== */
+#include "GlobalDefines.hpp"
+#include "FDM.hpp"
 #include "Grid.hpp"
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_ONLY_PNG
-#include "stb_image.h"
-#include "Utility.hpp"
+#include "GradientGrid.hpp"
+#include "Plot.hpp"
 
-#include <algorithm>
-#include <cstdio>
-
-bool
-Image::LoadImage(const char* path, const uint desiredComponents)
+// NOTE(Chris): It's going to be a bit of work to get tests running
+// again now
+#ifdef CATCH_CONFIG_MAIN
+#define LOG
+#else
+// NOTE(Chris): Provide logging for everyone - create in main TU to
+// avoid ordering errors.
+namespace Log
 {
-    int sizeX, sizeY, sizeN;
-    const u8* imageData = stbi_load(path, &sizeX, &sizeY, &sizeN, desiredComponents);
-    if (!imageData)
-    {
-        LOG("Image loading failed: %s", stbi_failure_reason());
-        return false;
-    }
-    data = std::shared_ptr<const u8*>(new const u8*(imageData), [](const u8** ptr)
-                                      {
-                                          if (*ptr)
-                                          {
-                                              // LOG("Freeing");
-                                              stbi_image_free(const_cast<u8*>(*ptr));
-                                          }
-                                      });
-
-    x = sizeX;
-    y = sizeY;
-    n = sizeN;
-    fileN = desiredComponents;
-    return true;
+    Lethani::Logfile log;
 }
+#endif
 
-bool
-Grid::LoadFromImage(const char* imagePath,
-                    const std::unordered_map<u32, Constraint>& colorMapping,
-                    uint scaleFactor)
+#if 0
+struct CommandLineFlags
 {
-    const Jasnah::Option<Image> image = LoadImage(imagePath, 4);
-    if (!image)
+    bool lastMatrix;
+    std::string infofilepath;
+};
+
+
+CommandLineFlags ParseArguments()
+{
+    try
     {
-        LOG("Loading failed");
-        // throw std::invalid_argument("Bad path or something");
-        return false;
-    }
+        // Aguments are in order: discription, seperation char, version number.
+        TCLAP::CmdLine cmd("Solving the electric and the voltage fields of a electrostatic prblem ",
+                           ' ', Version::Gridle);
 
-    ImageInfo info = image->GetInfo();
-    JasUnpack(info, pxPerLine, numScanlines);
+        // Aguments are in order: '-' flag, "--" flag,
+        // discription,add to an object(cmd line), default state.
+        TCLAP::SwitchArg lastMatrix("m", "lastMatrix", "Runs with the last matrix used",
+                                    cmd,false);
 
-    lineLength = pxPerLine;
-    numLines = numScanlines;
-    // const u32* rgbaData = (const u32*)data;
-    const u32* rgbaData = (const u32*)image->GetData();
+        // Aguments are in order: '-' flag, "--" flag,
+        // discription, default state, default string value , path, add to an object(cmd line).
+        TCLAP::ValueArg<std::string> infofile("i" , "infofile","holds the information about the matrix",
+                                              true, "", "path", cmd);
 
-    voltages.assign(pxPerLine * numScanlines, 0.0);
+        CommandLineFlags ret;
 
-    for (uint yLoc = 0; yLoc < numScanlines; ++yLoc)
-        for (uint xLoc = 0; xLoc < pxPerLine; ++xLoc)
+        ret.lastMatrix = lastMatrix.getValue();
+        ret.infofilepath = infofile.getValue();
+
+        if(lastMatrix)
         {
-            const RGBA texel = rgbaData[yLoc * lineLength + xLoc];
-            if (texel.rgba != Color::White)
-            {
-                auto iter = colorMapping.find(texel.rgba);
-                if (iter == colorMapping.end())
-                {
-                    LOG("Unknown color %u <%u, %u, %u, %u> encountered at (%u, %u), ignoring",
-                        texel.rgba, (unsigned)texel.r, (unsigned)texel.g,
-                        (unsigned)texel.b, (unsigned)texel.a, xLoc, yLoc);
-
-                    continue;
-                }
-
-                switch (iter->second.first)
-                {
-                case ConstraintType::CONSTANT:
-                    AddFixedPoint(xLoc, yLoc, iter->second.second); // Yes, this reeks of hack for now
-                    break;
-                case ConstraintType::OUTSIDE:
-                    // Do nothing - maybe set to constant 0?
-                    break;
-                case ConstraintType::LERP_HORIZ:
-                    // Need to scan and handle these later
-                    break;
-                case ConstraintType::LERP_VERTIC:
-                    // Need to scan and handle these later
-                    break;
-                default:
-                    LOG("Not yet implemented");
-                    break;
-                }
-            }
+            //TODO, get the previous matrix
+        }
+        else
+        {
+            //TODO,
         }
 
-    // Assuming only one horizontal lerp colour
-    // TODO(Chris): Make this check more rigourous
-    auto horizLerp = std::find_if(std::begin(colorMapping), std::end(colorMapping),
-                                  [](RemRefT<decltype(colorMapping)>::value_type val)
-                                  {
-                                      return val.second.first == ConstraintType::LERP_HORIZ;
-                                  });
 
-    if (horizLerp != colorMapping.end())
-    {
-        for (uint yLoc = 0; yLoc < numScanlines; ++yLoc)
-            for (uint xLoc = 0; xLoc < pxPerLine; ++xLoc)
-            {
-                const u32* texel = &rgbaData[yLoc * lineLength + xLoc];
-                if (*texel == horizLerp->first)
-                {
-                    uint len = 0;
-                    const uint maxLen = lineLength - xLoc;
-
-                    while (*texel == horizLerp->first && len <= maxLen)
-                    {
-                        ++texel;
-                        ++len;
-                    }
-                    // Texel now points to the pixel following the final lerp pixel
-
-                    // Need a pixel before and after the lerp for boundaries
-                    if (len == maxLen || xLoc == 0)
-                    {
-                        LOG("Lerp specification error, ignoring, make sure\n"
-                            "you have a constant pixel on each side");
-                        // continue outer loop
-                        break;
-                    }
-
-                    // Constants are already set at this point, so use them
-                    std::vector<f64> lerp = LerpNPointsBetweenVoltages(voltages[yLoc * lineLength + xLoc - 1],
-                                                                       voltages[yLoc * lineLength + xLoc + len],
-                                                                       len + 2);
-
-                    // Set the values
-                    for (uint i = 0; i < len+2; ++i)
-                    {
-                        AddFixedPoint(xLoc - 1 + i, yLoc, lerp[i]);
-                    }
-                }
-            }
     }
-    // Same can be done trivially for vertical lerp
-
-    // Do scaling here
-    if (!IsPow2(scaleFactor) && scaleFactor != 0)
+    catch(TCLAP::ArgException& ex)
     {
-        LOG("Image scale factor must be a power of 2 and non-zero, ignoring");
-        scaleFactor = 1;
+        LOG("Parsing error %s for arg %s", ex.error().c_str(), ex.argId().c_str());
     }
-
-    if (scaleFactor != 1)
-    {
-        // DoubleVec scaledImage;
-        decltype(voltages) scaledImage;
-        // Reserve new size
-        // scaledImage.reserve(Square(scaleFactor) * voltages.size());
-
-        // Set new dimensions
-        numLines *= scaleFactor;
-        lineLength *= scaleFactor;
-
-        // set new image to 0
-        scaledImage.assign(numLines * lineLength, 0.0);
-
-        std::swap(voltages, scaledImage);
-
-        // Assign scaled fixed points from previous fixed points
-        decltype(fixedPoints) fp;
-        std::swap(fixedPoints, fp);
-
-        for (uint y = 0; y < numLines; ++y)
-        {
-            for (uint x = 0; x < lineLength; ++x)
-            {
-                // unscaled is the index that this point had in the original unscaled image
-                const uint unscaled = y / scaleFactor * lineLength / scaleFactor + x / scaleFactor;
-
-                // See if unscaled is in the list of fixed points
-                auto found = fp.find(unscaled);
-
-                // If it is add (x,y) to the new list of fixed points
-                if (found != fp.end())
-                {
-                    AddFixedPoint(x, y, found->second);
-                }
-            }
-        }
-    }
-    return true;
 }
+#endif
 
-/// Sets the two boundary plates for the basic box
-void
-Grid::InitialiseBasicGrid(const f64 plusWall, const f64 minusWall)
+#ifndef CATCH_CONFIG_MAIN
+int main(void)
 {
-    voltages.assign(lineLength*numLines, 0.0);
+    // This looks better and far more generic
+    std::unordered_map<u32, Constraint> colorMap;
+    colorMap.emplace(Color::Black, std::make_pair(ConstraintType::CONSTANT, 0.0));
+    colorMap.emplace(Color::Red, std::make_pair(ConstraintType::CONSTANT, 10.0));
+    colorMap.emplace(Color::PaintRed, std::make_pair(ConstraintType::CONSTANT, 10.0));
+    colorMap.emplace(Color::Blue, std::make_pair(ConstraintType::CONSTANT, -10.0));
+    colorMap.emplace(Color::Green, std::make_pair(ConstraintType::LERP_HORIZ, 0.0));
+    // Grid grid("prob1.png", colorMap);
+    Grid grid;
+    grid.LoadFromImage("prob0.png", colorMap, 8);
 
-    for (MemIndex i = 0; i < numLines; ++i)
-    {
-        AddFixedPoint(0, i, plusWall);
-        AddFixedPoint(lineLength - 1, i, minusWall);
-    }
-}
+    // NOTE(Chris): Staggered leapfrog seems to follow a 1/(x^2) convergence, we can exploit this...
+    // SolveGridLaplacianZero(&grid, 0.0001, 10000);
+    FDM::SolveGridLaplacianZero(&grid, 0.001, 10000);
 
-/// Prints the grid to stdout, useful for debugging at low res
-void
-Grid::Print()
-{
-    for (uint i = 0; i < numLines; ++i)
-    {
-        for (uint j = 0; j < lineLength; ++j)
-        {
-            // The code in here is to centre the value within a 7
-            // space width as much as possible
-            const MemIndex maxBuf = 8;
-            char buf[maxBuf];
-            const uint len = snprintf(buf, maxBuf, "%1.1g", voltages[i*lineLength + j]);
-            for (uint i = 0; i < (maxBuf-len-1)/2; ++i)
-            {
-                char tempBuf[maxBuf];
-                memcpy(tempBuf, buf, maxBuf);
-                snprintf(buf, maxBuf, " %s ", tempBuf);
-            }
-            if ((maxBuf - len - 1) & 1)
-            {
-                char tempBuf[maxBuf];
-                memcpy(tempBuf, buf, maxBuf);
-                snprintf(buf, maxBuf, "%s ", tempBuf);
-            }
-            printf("%s", buf);
-        }
-        printf("\n");
-    }
+    const f64 cellsToMeters = 100.0;
+    GradientGrid grad;
+    grad.CalculateNegGradient(grid, cellsToMeters);
+
+    using namespace Plot;
+
+    WriteGridForGnuplot(grid);
+    WriteGnuplotColormapFile(grid);
+    WriteGnuplotContourFile(grid);
+
+    WriteGradientGridForGnuplot(grad);
+    WriteGnuplotGradientFile(grad, 0.08);
+
+    return 0;
 }
+#endif
